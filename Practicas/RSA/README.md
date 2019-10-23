321fcdd3ba78a80ec76da235395374e79224f698
DamianRi <dams_rive@ciencias.unam.mx>
2019-10-23 12:28:14 -0500

    Subiendo base de RSA

---
+++ Practicas/RSA/README.md
@@ -0,0 +1,10 @@
+## Criptografía y Seguridad
+## Proyecto 2: RSA
+
+### Descripción
+Código implementado con Python 3.7.5
+
+
+### Contenido
+
+### Ejecución 
---
+++ Practicas/RSA/RSA.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import random
+from Auxiliares import ex
+from Auxiliares import inverso
+from Auxiliares import mcd
+from Crypto.Util import number
+
+def generar_publica(fi, n):
+    """Funcion que genera la llave publica a partir de la funcion de euler y n"""
+    # se elige un entero e entre 2 y fi tal que sean coprimos
+    e = random.randrange(2, fi)
+
+    # se calcula el maximo comun divisor para verificar si son coprimos
+    #  de no serlo se elige otro entero e hasta encontrar alguno
+    g = mcd(e, fi)
+    while g != 1:
+        e = random.randrange(2, fi)
+        g = mcd(e, fi)
+
+    # Se regresan los valores encontrados
+    return e, n
+
+
+def generar_privada(e, fi, n):
+    """Funcion que genera la llave privada a partir del e y fi"""
+    # Se calcula el inverso multiplicativo de e
+    d = inverso(e, fi)
+
+    return d, n
+
+
+def codificar(e, n, mensaje):
+    """Funcion que codifica un mensaje dada la llave publica"""
+    # Se toma cada caracter del mensaje y se codifica con la llave publica
+    # C    =       m      ^  e (mod n)
+    mensaje_codificado = [ex(ord(char), e, n) for char in mensaje]
+    # Regresa la lista con los numeros que representan a cada caracter del mensaje
+    return mensaje_codificado
+
+
+def decodificar(d, n, mensaje_codificado):
+    """Funcion que decodifica un mensaje dada la llave privada"""
+    # Se toma cada numero en la lista del mensaje_codificado y se decodifica con la llave privada
+    # m   =       c    ^   d (mod n)
+    mensaje = [chr(ex(char, d, n)) for char in mensaje_codificado]
+    # Regresa la cadena formada por los caracteres de la lista
+    return ''.join(mensaje)
+
+
+"""
+Integrantes:
+    Tadeo Guillén Diana G
+    Rivera González Damián
+"""
+if __name__ == '__main__':
+    print ("Algoritmo RSA\nGenerando primos...")
+
+    while True:
+        
+        p = number.getPrime(330)
+        q = number.getPrime(330)
+
+        # Dejamos de generar hasta que sean dos primos diferentes
+        if p != q:
+            break
+
+    print ("p = ", p)
+    print ("q = ", q)
+
+    n = p * q
+    # Se calcula la funcion de Euler
+    fi = (p - 1) * (q - 1)
+
+    print ("\nGenerando llaves...")
+    e, n = generar_publica(fi, n)
+    print ("Llave publica:\ne = ", e, "\nn = ", n)
+
+    d, n = generar_privada(e, fi, n)
+    print ("Llave privada:\nd = ", d, "\nn = ", n)
+
+    print ("\nProbando la codificacion con llave publica...")
+    mensaje = input("Ingresa un mensaje: ")
+    print ("Codificando con llave publica...")
+    mensaje_codificado = codificar(e, n, mensaje)
+    print ("Mensaje codificado:\n", ''.join(map(lambda x: str(x), mensaje_codificado)))
+
+    print ("\nProbando la decodificacion con llave privada...")
+    print ("Decodificando con llave privada...")
+    mensaje_decodificado = decodificar(d, n, mensaje_codificado)
+    print ("Mensaje decodificado:\n", mensaje_decodificado)
