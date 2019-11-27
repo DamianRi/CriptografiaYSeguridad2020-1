69a9fe10a5b6de77794d1a7a3aabe5df96d584a1
DamianRi <dams_rive@ciencias.unam.mx>
2019-11-27 12:45:04 -0600

    Agregando pocos comentarios

--- Practicas/Lenstra/src/Lenstra.py
+++ Practicas/Lenstra/src/Lenstra.py
@@ -17,14 +17,14 @@ De otra manera se deja el enlace para su instalación [**PyCryptodome**](https:/
 
 - - -
 """
-
-
 from Crypto.Util import number
+import random
 import sys
+import math
 
 #num = 1073561597    // Número a factorizar
 #NUM = 87463
-NUM = 35
+NUM = 455839
 
 class Punto:
     
@@ -38,6 +38,9 @@ class Punto:
 def modn(x):
     return (x+NUM)%NUM
 
+"""
+    Función para realzar la suma entre dos Puntos diferentes
+"""
 def suma_puntos_diferentes(p, q):
     pr = Punto(0,0)
 
@@ -50,11 +53,14 @@ def suma_puntos_diferentes(p, q):
 
     pr.lam = modn((q.y-p.y)*number.inverse(q.x - p.x, NUM))
     pr.num = modn(q.y - p.y)
-    pr.den = modn(q.x + p.x)
+    pr.den = modn(q.x - p.x)
     pr.x = modn (modn (pr.lam * pr.lam) + modn ((-1 * p.x)) + modn ((-1 * q.x)))
     pr.y = modn ((pr.lam * (p.x - pr.x)) - p.y)
     return pr
 
+"""
+    Función para realizar la suma entre dos Puntos iguales
+"""
 def suma_puntos_iguales(p):
     pr = Punto(0,0)
     pr.lam = modn (((3 * (p.x * p.x)) + a) * number.inverse(p.y * 2, NUM))
@@ -64,15 +70,24 @@ def suma_puntos_iguales(p):
     pr.y = modn ((pr.lam * (p.x - pr.x)) - p.y)
     return pr
 
+"""
+    Función para hacer la suma de puntos
+    Dados dos puntos p y q realiza la suma de estos
+"""
 def suma_puntos(p, q):
     if son_iguales(p, q):
         return suma_puntos_iguales(p)
     return suma_puntos_diferentes(p, q)
 
+"""
+    Función auxiliar para verificar si los puntos son iguales
+"""
 def son_iguales(p,q):
     return (p.x == q.x) and (p.y == q.y)
 
-
+"""
+    Función para realizar k veces la suma de un punto p
+"""
 def multiplica_k_punto(k, p):
     pr = Punto(0,0)
     pt = Punto(0,0)
@@ -83,25 +98,32 @@ def multiplica_k_punto(k, p):
 
     return pr
 
-if __name__ == "__main__":
-    p_inicial = Punto(1,1)
-    pt = Punto(0,0)
 
-    mcd1 = 0
-    #NUM = int(input("Número a Factorizar:"))
-    k = int(input("Ingresa un valor para K:"))
-    a = int(input("Ingresa un valor para A:"))
+if __name__ == "__main__":
+
+    mcd = 0
+    NUM = int(input("Número a Factorizar:"))
+    k = int(input("Número máximo de iteraciones (k): "))
+    #a = int(input("Ingresa un valor para A:"))
+    a = random.randrange(1, NUM)
     B = a*(-1)
 
-    #   Se Realiza la iteración desde 2 hasta k
-    for i in range(2, k):
-        pt = multiplica_k_punto(i, p_inicial)
-        mcd1 = number.GCD(pt.den, NUM)
+    # Un punto no trivial en la curva
+    p_inicial = Punto(1,1)
 
-        if mcd1 != 1:
-            print("\n\t=>  %d es un factor de %d" %(mcd1, NUM))
-            q = NUM/mcd1
-            print("\t=>  %d = %d*%d" %(NUM, mcd1, q))
+    pt = Punto(0,0)
+    print("Curva Elíptica: y^2 = x^3 +",a,"x",B)
+
+    #   Se Realiza la iteración desde a=2 hasta k
+    for a in range(2, k):
+        pt = multiplica_k_punto(a, p_inicial)   # Calculamos kP
+
+        # Obtenemos el máximo común divisor
+        mcd = number.GCD(pt.den, NUM)
+        if mcd != 1:    # Si no son primos
+            print("\n\t=>  %d es un factor de %d" %(mcd, NUM))
+            q = NUM/mcd
+            print("\t=>  %d = %d*%d" %(NUM, mcd, q))
             quit()
 
     print("\n\tXx No se encontro un factor de %d" %(NUM))
